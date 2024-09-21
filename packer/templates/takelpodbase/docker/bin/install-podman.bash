c36d7805fd4267c3f6754afd8187c7cdfbf06b83
Stephan <stephan@takelwerk.net>
2024-09-21 17:16:36 +0200

    Initial commit

---
+++ packer/templates/takelpodbase/docker/bin/install-podman.bash
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+apt-get -y update
+apt-get -y dist-upgrade
+apt-get install -y --no-install-recommends python3 python3-apt sudo systemd
+apt-get --yes --no-install-recommends install \
+  ca-certificates \
+  libvshadow-utils \
+  passt \
+  podman \
+  podman-compose \
+  python3-minimal \
+  python3-apt \
+  slirp4netns \
+  sudo \
+  systemd \
+  uidmap
+apt-get clean
+rm -fr /sbin/initctl
+cat > /sbin/initctl <<sbin_initctl
+#!/bin/sh
+ALIAS_CMD="\$(echo ""\$0"" | sed -e 's?/sbin/??')"
+case "\$ALIAS_CMD" in
+    start|stop|restart|reload|status)
+        exec service \$1 \$ALIAS_CMD
+        ;;
+esac
+case "\$1" in
+    list )
+        exec service --status-all
+        ;;
+    reload-configuration )
+        exec service \$2 restart
+        ;;
+    start|stop|restart|reload|status)
+        exec service \$2 \$1
+        ;;
+    \?)
+        exit 0
+        ;;
+esac
+sbin_initctl
+chmod +x /sbin/initctl
+/usr/sbin/useradd \
+  --comment 'podman user to run rootless containers' \
+  --home-dir /home/podman \
+  --create-home \
+  --shell /bin/bash \
+  --user-group \
+  podman
+su podman -c 'mkdir -p /home/podman/.config/containers'
+su podman -c 'echo -e "[storage]\ndriver = \"vfs\"\n" > /home/podman/.config/containers/storage.conf'
---
+++ packer/templates/takelpodbase/docker/main.pkr.hcl
@@ -0,0 +1,51 @@
+packer {
+  required_plugins {
+    ansible = {
+      version = "~> 1"
+      source = "github.com/hashicorp/ansible"
+    }
+    docker = {
+      source  = "github.com/hashicorp/docker"
+      version = "~> 1"
+    }
+  }
+}
+
+source "docker" "takelpodbase" {
+  # export_path = "images/docker/${var.target_repo}.tar"
+  image = "${var.base_repo}:${var.base_tag}"
+  commit = true
+  pull = false
+  run_command = [
+    "--detach",
+    "--interactive",
+    "--tty",
+    "--name",
+    "${var.target_repo}",
+    "${var.base_user}:${var.base_tag}",
+    "/bin/bash"
+  ]
+  changes = [
+    "WORKDIR /root",
+    "ENV DEBIAN_FRONTEND=noninteractive",
+    "ENV LANG=C.UTF-8",
+    "ENV SUPATH=$PATH",
+    "ENV PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+    "CMD [\"/lib/systemd/systemd\"]"
+  ]
+}
+
+build {
+  sources = [
+    "source.docker.takelpodbase"
+  ]
+
+  provisioner "shell" {
+    script = "${var.packer_template_dir}/bin/install-podman.bash"
+  }
+
+  post-processor "docker-tag" {
+    repository = "${var.local_user}/${var.target_repo}"
+    tags = ["${var.target_tag}"]
+  }
+}
